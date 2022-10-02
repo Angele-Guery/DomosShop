6cf3e9108ebc4785d0377f6a1bbb716a76d79fee
Angele-Guery <55349187+Angele-Guery@users.noreply.github.com>
2022-10-02 18:38:59 +0200

    routing done

--- domos-shop-front/src/app/app-routing.module.ts
+++ domos-shop-front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cart', component: CartComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', component: ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- domos-shop-front/src/app/components/products/products.component.ts
+++ domos-shop-front/src/app/components/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { products } from './predefined-products';
-import {ShoppingCartService} from '../../services/shopping-cart.service'
+import {ShoppingCartService} from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-products',
