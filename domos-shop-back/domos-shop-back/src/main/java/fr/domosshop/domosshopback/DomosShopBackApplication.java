c54e970a803127c38154374337ba40357e73b88b
Angele-Guery <55349187+Angele-Guery@users.noreply.github.com>
2022-10-07 21:11:05 +0200

    back

---
+++ domos-shop-back/domos-shop-back/src/main/java/fr/domosshop/domosshopback/DomosShopBackApplication.java
@@ -0,0 +1,13 @@
+package fr.domosshop.domosshopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomosShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomosShopBackApplication.class, args);
+	}
+
+}
---
+++ domos-shop-back/domos-shop-back/src/test/java/fr/domosshop/domosshopback/DomosShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.domosshop.domosshopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomosShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
